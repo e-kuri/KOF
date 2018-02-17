dc891de1f361bfee3af045671ae50817338ffdf9
e-kuri <eugenio.kuri@gmail.com>
2018-02-17 13:35:46 -0600

    added empty component for firestore

---
+++ Components/Chat/ChatRoom/ChatRoom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import firebase from 'react-native-firebase';
+
+class ChatRoom extends React.Component {
+    render(){
+        return null;
+    }
+}
+
+export default  ChatRoom;
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('KOF', () => App);
+import ChatRoom from './Components/Chat/ChatRoom/ChatRoom'
+AppRegistry.registerComponent('KOF', () => ChatRoom);
